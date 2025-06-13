a2237b226e03accb8425e0a0a7b7f94d725dabb5
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-06-13 19:22:16 +0900

    feat: optimize the model

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 # CONFIGURATION
 CONFIG = {
-  "bias": False,
-  "num_heads": 8,
+  "num_heads": 2,
+  "dim": 2 * 32,  # num_heads * 8
+  "iterations": 10,
+  "batch_size": 32,
+  "learning_rate": 0.001,
+  "pretrained_model": "bert-base-uncased",
 } # CONFIG
--- src/model/AttentionNet.py
+++ src/model/AttentionNet.py
@@ -1,27 +1,30 @@
+from datasets import load_dataset
 import torch.optim
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from src.BPEDataset import load_tokenizer, BPEDataset
+
+
 
 class AttentionNet(nn.Module):
-  def __init__(self, num_heads: int, dim: int, bias: bool, oupt_dim: int):
+  def __init__(self, num_heads: int, dim: int, oupt_dim: int, bias: bool = False):
     super(AttentionNet, self).__init__()
-    self.num_heads, self.dim, self.bias = num_heads, dim, bias
+    self.num_heads, self.dim, self.bias, self.gamma = num_heads, dim, bias, 0.1
 
-    self.at1 = MultiHeadAttention(dim=self.dim, num_heads=8, bias=self.bias)
-    self.fc1 = nn.Linear(self.dim, self.dim, bias=self.bias)
-    self.at2 = MultiHeadAttention(dim=self.dim, num_heads=8, bias=self.bias)
-    self.fc2 = nn.Linear(self.dim, oupt_dim, bias=self.bias)
+    self.at1 = MultiHeadAttention(dim=self.dim, num_heads=self.num_heads, bias=self.bias)
+    self.fc1 = nn.Linear(self.dim, oupt_dim, bias=self.bias)
     self.swish, self.softmax = nn.SiLU(), nn.Softmax(dim=-1)
+    self.norm, self.dropout = nn.LayerNorm(dim), nn.Dropout(p=self.gamma)
   # __init__()
 
   def forward(self, x):
-    x = self.at1(x)
+    x = x + self.dropout(self.at1(self.norm(x)))
     x = self.swish(x)
     x = self.fc1(x)
-    x = self.at2(x)
-    x = self.swish(x)
-    x = self.fc2(x)
-    return self.softmax(x)
+    return self.swish(x)
   # forward()
 # AttentionNet
 
@@ -44,11 +47,41 @@ class MultiHeadAttention(nn.Module):
   # attn_score()
 # MultiHeadAttention
 
+def init_weights(m):
+  if isinstance(m, nn.Linear):
+    nn.init.xavier_uniform_(m.weight)
+    if m.bias is not None:
+      nn.init.zeros_(m.bias)
+# init_weights()
+
 if __name__ == "__main__":
   from config import CONFIG
 
-  model = AttentionNet(num_heads=CONFIG["num_heads"], dim=CONFIG["dim"], bias=CONFIG["bias"])
-  x = torch.randn(1, 10, CONFIG["dim"])
-  output = model(x)
-  print(output.shape)
+  # Initialize device and model parameters
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+  # Load dataset and tokenizer
+  dataset = load_dataset("imdb")["train"]
+  tokenizer, pretrained_model = load_tokenizer("../tokenizer.json", PRETRAINED_MODEL=CONFIG["pretrained_model"])
+  tokenizer.enable_truncation(max_length=CONFIG["dim"])
+  tokenizer.enable_padding(pad_id=tokenizer.token_to_id("[PAD]"), pad_token="[PAD]", length=CONFIG["dim"])
+  trainset = BPEDataset(dataset=dataset, encode=(tokenizer, pretrained_model), dim=CONFIG["dim"])
+
+  # Initialize model, criterion, and optimizer
+  model = AttentionNet(num_heads=CONFIG["num_heads"], dim=CONFIG["dim"], bias=False, oupt_dim=len(trainset.num_classes))
+  model.to(device)
+  model.apply(init_weights)
+  nn.utils.clip_grad_norm(model.parameters(), max_norm=1.0)
+  criterion, optim = nn.CrossEntropyLoss(), torch.optim.Adam(model.parameters(), lr=CONFIG["learning_rate"])
+
+  # Training loop
+  progress = tqdm(range(CONFIG["iterations"]), desc="training", unit="epoch", leave=True, dynamic_ncols=True)
+  for _ in progress:
+    for feature, label in DataLoader(trainset, batch_size=CONFIG["batch_size"], shuffle=True, pin_memory=True, num_workers=4):
+      feature, label = feature.to(device, non_blocking=True), label.to(device, non_blocking=True)
+      output = model.forward(feature)
+      loss = criterion(output, label)
+      optim.zero_grad()
+      loss.backward()
+      optim.step()
 # if __name__ == "__main__":
