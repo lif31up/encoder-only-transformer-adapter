166904349249b8003960f06a6d8a64b80e4d7d25
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-06-13 10:24:09 +0900

    feat: adapt to data structure

--- src/BPEDataset.py
+++ src/BPEDataset.py
@@ -1,20 +1,24 @@
 import torch
 from tokenizers import Tokenizer, models, trainers
 from transformers import AutoModel
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 
 class BPEDataset(Dataset):
-  def __init__(self, dataset, transforms):
-    self.dataset, self.transforms = dataset, transforms
-    self.dim = self.__getitem__(0).shape[1]
+  def __init__(self, dataset, encode):
+    self.dataset, self.encode = dataset, encode
+    self.num_classes = list(set(dataset["label"]))
+    self.dim = self.__getitem__(0)[0].shape[1]
   # __init__()
+
+  def __len__(self): return len(self.dataset)
 
   def __getitem__(self, item):
     assert item < len(self.dataset), "Index out of bounds"
-    returned_item = self.dataset[item]
-    if self.transforms:
-      for transform in self.transforms: returned_item = transform(returned_item)
-    return returned_item
+    feature, label = self.dataset["text"][item], self.dataset["label"][item]
+    label = F.one_hot(torch.tensor(self.num_classes.index(label)), num_classes=len(self.num_classes)).float()
+    if self.encode: feature = embed(feature, *self.encode)
+    return feature, label
 # Dataset
 
 def create_tokenizer(DATASET, PATH: str):
@@ -32,10 +36,10 @@ def load_tokenizer(PATH: str, PRETRAINED_MODEL: str):
   return Tokenizer.from_file(PATH), AutoModel.from_pretrained(PRETRAINED_MODEL)
 # load_tokenizer(): Loads a tokenizer from a specified file path and initializes a pre-trained model.
 
-def embed(text: str, tokenizer: Tokenizer, model: AutoModel):
-  input_ids = torch.tensor([tokenizer.encode(text).ids])
-  with torch.no_grad(): outputs = model(input_ids)
-  return torch.mean(outputs.last_hidden_state, dim=1)
+def embed(text: str, tokenizer: Tokenizer, pretrained_model: AutoModel):
+  input_ids = torch.tensor([tokenizer.encode(text, add_special_tokens=True).ids])
+  with torch.no_grad(): outputs = pretrained_model(input_ids)
+  return torch.mean(outputs.last_hidden_state, dim=1, dtype=torch.float32)
 # embed(): Encodes a given text using the tokenizer and computes the mean of the last hidden state from the model's output.
 
 if __name__ == "__main__":
@@ -47,12 +51,13 @@ if __name__ == "__main__":
     print(f"Tokenizer created and saved to {save_path}.")
   if tag == "load_tokenizer":
     tokenizer, model = load_tokenizer(save_path, "bert-base-uncased")
-    print(embed("your text here", tokenizer, model).shape)
+    print(embed("your text here", tokenizer, model))
   if tag == "dataset":
     from datasets import load_dataset
-    dataset = load_dataset("imdb")["train"]["text"]
+    dataset = load_dataset("imdb")["train"]
     tokenizer, pretrained_model = load_tokenizer("tokenizer.json", PRETRAINED_MODEL="bert-base-uncased")
-    transforms = [lambda x: embed(x, tokenizer, pretrained_model)]
-    trainset = BPEDataset(dataset=dataset, transforms=transforms)
-    print(trainset[0])
+    trainset = BPEDataset(dataset=dataset, encode=(tokenizer, pretrained_model))
+    for feature, label in DataLoader(trainset, batch_size=4, shuffle=True, pin_memory=True, num_workers=4):
+      print(feature.shape, label.shape)
+      break
 # __name__
