bfa9f4a543da0b6ad0a9ede36d4241ca938423ee
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-06-22 19:40:21 +0900

    Update BERT.py

--- src/model/BERT.py
+++ src/model/BERT.py
@@ -6,13 +6,12 @@ class BERT(nn.Module):
   def __init__(self, config, init_weights=None):
     super(BERT, self).__init__()
     self.stacks = nn.ModuleList([EncoderStack(config, init_weights=init_weights) for _ in range(config["n_stack"])])
-    self.fc = nn.Linear(config["dim"], config["oupt_dim"], bias=config["bias"])
-    self.softmax, self.avg_pool = nn.Softmax(dim=-1), nn.AvgPool1d(1)
+    self.fc ,self.flatten = nn.Linear(393216, config["oupt_dim"], bias=config["bias"]), nn.Flatten(1)
   # __init__():
 
   def forward(self, x):
     for stack in self.stacks: x = stack(x)
-    return self.fc(self.avg_pool(x))
+    return self.fc(self.flatten(x))
   # forward()
 # BERT
 
@@ -31,14 +30,9 @@ if __name__ == "__main__":
   bert_model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True, output_attentions=False)
   trainset = BPEDataset(dataset=dataset, dim=model_config["dim"], tokenizer=bert_tokenizer, model=bert_model)
 
-  def init_weights(m):
-    if isinstance(m, nn.Linear):
-      nn.init.xavier_uniform_(m.weight)
-      if m.bias is not None: nn.init.zeros_(m.bias)
-  # init_weights()
   vurt = BERT(model_config, init_weights=None)
 
-  for feature, label in DataLoader(trainset, batch_size=2, shuffle=True, pin_memory=True, num_workers=4):
+  for feature, label in DataLoader(trainset, batch_size=2, shuffle=True, pin_memory=True, num_workers=1):
     output = vurt(feature)
     print(f"Feature shape: {feature.shape}, Label shape: {label.shape}")
     print(f"Output shape: {output.shape}")
